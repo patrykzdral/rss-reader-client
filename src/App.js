d4ccc061e075c4024ab1878bfd6b0d227fadc635
Patryk Zdral <patrykz13@gmail.com>
2020-04-04 23:48:41 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React by Patryk Zdral
         </a>
       </header>
     </div>
